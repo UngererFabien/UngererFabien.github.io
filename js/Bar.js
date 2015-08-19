742c92d3828f86b2976a389f002bbbf84512952f
UngererFabien <fabien.ungerer@gmail.com>
2015-08-19 10:39:54 +0200

    add charts

---
+++ js/Bar.js
@@ -0,0 +1,157 @@
+d3.chart('BarChart', {
+
+  initialize: function () {
+
+    var svg = this.base.node(),
+      width = +svg.getAttribute('width'),
+      height = +svg.getAttribute('height');
+
+    var nbBar = 10,
+      barSpace, barWidth, barMargin;
+
+    barSpace = width / nbBar;
+    barMargin = width / ((nbBar * 2) + nbBar + 1);
+    barWidth = barMargin * 2;
+
+    this.scaleX = d3.scale.linear()
+      .domain([0, 1])
+      .range([0, barSpace]);
+
+    this.scaleY = d3.scale.linear()
+      .range([height, 0]);
+
+    // a new group element on the base of the chart
+    this.layer('VerticalBars', this.base.append('g'), {
+
+      // select the elements we wish to bind to and
+      // bind the data to them.
+      dataBind: function (data) {
+        console.log(data);
+        return this.selectAll('.bar')
+          .data(data, function (point, i) {
+            // console.log('data bind : ', point.id, i);
+            return point.id;
+          });
+      },
+
+      insert: function () {
+        return this.append('rect')
+          .attr('class', 'bar');
+      },
+
+      // define lifecycle events
+      events: {
+        'first:enter': function () {
+          var chart = this.chart(),
+            maxIndex = this.size() - 1;
+
+          this.attr('x', function (point, i) {
+              return (i == maxIndex ? chart.scaleX(i + 1) : chart.scaleX(i)) + barMargin / 2;
+            }).attr('width', barWidth)
+            .attr('y', function (point) {
+              // return height-1; // For animation
+              return chart.scaleY(point.value) + 1;
+            }).attr('height', function (point) {
+              // return 1 // For animation
+              return height - chart.scaleY(point.value);
+            });
+        },
+
+        enter: function () {
+          var chart = this.chart();
+
+          this.attr('x', function (point, i) {
+              return chart.scaleX(i + 1) + barMargin;
+            }).attr('width', barWidth)
+            .attr('y', function (point) {
+              // return height-1; // For animation
+              return chart.scaleY(point.value) + 1;
+            }).attr('height', function (point) {
+              // return 1 // For animation
+              return height - chart.scaleY(point.value);
+            });
+
+        },
+
+        'enter:transition': function () {
+
+        },
+
+        update: function () {
+
+        },
+
+        'update:transition': function () {
+
+        },
+
+        exit: function () {
+
+        },
+
+        'exit:transition': function () {
+          //console.log('exit', this);
+
+          var chart = this.chart();
+
+          this.duration(1000)
+            .attr('x', function (point, i) {
+              return chart.scaleX(i - 1);
+            }).remove();
+        },
+
+        merge: function () {
+
+        },
+
+        'merge:transition': function () {
+          // console.log('merge:transition');
+
+          var chart = this.chart();
+
+          // Keep this animation for Y rescaling
+          this.duration(1000)
+            .attr('x', function (point, i) {
+              return chart.scaleX(i) + barMargin / 2;
+            }).attr('y', function (point) {
+              return chart.scaleY(point.value) + 1;
+            }).attr('height', function (point) {
+              return height - chart.scaleY(point.value);
+            });
+        }
+      }
+    });
+
+    this.base.append('svg:line')
+      .attr('x1', 0)
+      .attr('x2', width)
+      .attr('y1', height)
+      .attr('y2', height)
+      .attr('stroke', '#000');
+  },
+
+  transform: function (data) {
+    var max = d3.max(data, function (point) {
+      return point.value;
+    });
+
+    // this.scaleX.domain(data.map(function (point) {
+    //   return point.id;
+    // }));
+
+    this.scaleY.domain([0, max]);
+
+    return data;
+  },
+
+  // set/get the color to use for the circles as they are
+  // rendered.
+  color: function (newColor) {
+    if (arguments.length === 0) {
+      return this._color;
+    }
+    this._color = newColor;
+    return this;
+  }
+
+});
---
+++ js/Bubbles.js
@@ -0,0 +1,143 @@
+d3.chart('BubblesChart', {
+
+  initialize: function  () {
+    var _this = this;
+    this.dataDepth = 1;
+
+    var svg = this.base.node(),
+      width = +svg.getAttribute('width'),
+      height = +svg.getAttribute('height');
+
+    var diameter = d3.min([width, height]);
+
+    var pack = d3.layout.pack().size([diameter, diameter])
+      .value(function (d) {
+        return d.value;
+      })
+      .children(function (d) {
+        if(d.depth == _this.dataDepth) {
+          return d.sumedChildren || d._children;
+        } else return d._children;
+      });
+      // .padding(3);
+
+    this.layer('Bubbles', this.base.append('g'), {
+
+      dataBind: function(data) {
+        return this.datum(data).selectAll('.bubble')
+          .data(pack.nodes, function (node) {
+            return node.id;
+          });
+      },
+
+      insert: function() {
+        var chart = this.chart();
+
+        return this.append('g')
+          .attr('class', function (d) {
+            return 'bubble'//+ (d.depth < 1 ? 'zoomable' : '');
+          }).attr('id', function (d) {
+            return d.id;
+          }).attr('data-zoomId', function (d) {
+            return d.depth > 1 || d.id.indexOf('sumed') > 0 ? d.parent.id : d.id;
+          }).on('click', function (d) {
+            if (d.depth == 0) {
+              if(chart.dataDepth == 0 || d.parent) {
+                chart.zoom(1, true);
+              } else {
+                chart.zoom(0, true);
+              }
+
+              if(d.parent) {
+                chart.draw(d.parent);
+              } else {
+                chart.draw(d);
+              }
+
+            } else {
+              if(d.depth == chart.dataDepth){
+                if(d._children) {
+                  chart.draw(d);
+                } else {
+                  chart.draw(d.parent.parent);
+                }
+              } else if(d.depth > chart.dataDepth && d.parent) {
+                if(d.parent.depth == 0) {
+                  chart.zoom(1, true);
+                }
+
+                chart.draw(d.parent);
+              }
+            }
+          }).on('mouseenter', function () {
+            var zoomId = d3.select(this).attr('data-zoomId');
+            d3.select('#'+zoomId).attr('class', 'bubble zoomHover')
+          }).on('mouseleave', function () {
+            var zoomId = d3.select(this).attr('data-zoomId');
+            d3.select('#'+zoomId).attr('class', 'bubble')
+          });
+      },
+
+      // define lifecycle events
+      events: {
+        enter: function () {
+          var chart = this.chart();
+
+          this.attr('transform', function(d) {
+            //console.log(d);
+            return 'translate('+ d.x + ',' + d.y + ')';
+          });
+
+          this.append('circle')
+            // .attr('class', function (d) {
+            //   return d.depth <= 1 ? 'zoomable' : '';
+            // })
+            .attr('r', function (d) {
+              return 0;
+            });
+        },
+
+        exit: function () {
+          this.remove();
+        },
+
+        'merge:transition': function () {
+          // this.duration(500).attr('transform', function (d) {
+          //   return 'translate('+ d.x + ',' + d.y + ')';
+          // });
+
+          this.each(function (d) {
+            var g = d3.select(this),
+              circles = g.selectAll('circle');
+
+            var rayon = circles.attr('r'),
+              posDuration = 500, sizeDuration = 500, 
+              posDelay = sizeDuration*(rayon > d.r ? 1 : 0),
+              sizeDelay = 200*d.depth*(rayon > d.r ? 0 : 1);
+
+            g.transition().delay(0).duration(posDuration).attr('transform', function (d) {
+              return 'translate('+ d.x + ',' + d.y + ')';
+            });
+            circles.transition().delay(sizeDelay).duration(sizeDuration).attr('r', d.r);
+          });
+        }
+      }
+
+    });
+  },
+
+  transform: function (data) {
+    return data;
+  },
+
+  zoom: function (z, reset) {
+    if(reset) {
+      this.dataDepth = z;
+    } else {
+      this.dataDepth += z;
+    }
+
+    return this;
+  }
+
+});
