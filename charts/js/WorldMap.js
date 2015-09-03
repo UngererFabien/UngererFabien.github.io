ed15a58fed3957ac8c0e0825500459a98db826d4
UngererFabien <fabien.ungerer@gmail.com>
2015-09-03 15:20:02 +0200

    this is a map now

--- charts/js/WorldMap.js
+++ charts/js/WorldMap.js
@@ -18,37 +18,60 @@ d3.chart('WorldMapChart', {
 
     var initTranslate = [0, -100];
 
-    var translate, scale, area; // variables for zoom
-
-    var clip = d3.geo.clipExtent();
-
-    var simplify = d3.geo.transform({
-      point: function (x, y, z) {
-        if(z >= area) {
-          this.stream.point(x * scale + translate[0], y * scale + translate[1]);
-        }
-      }
-    });
-
-    var zoom = d3.behavior.zoom()
-      .translate(initTranslate)
-      .scale(1)
-      .scaleExtent([1, 8]);
-
-    var path = d3.geo.path()
-      .projection({
-        stream: function (s) {
-          return simplify.stream(clip.stream(s));
-        }
-      })
-      .context(context);
-
     // map generation commande : 
     // topojson -q 1e5 --projection='width = 1080, height = 720, d3.geo.equirectangular().translate([width/2, height/2]).scale((width) / 2 / Math.PI)' countries=ne_50m_admin_0_countries.shp -o world.json
     
     // map.json values
     var mapWidth = 1080,
       mapHeight = 720;
+
+    // var translate, scale, area; // variables for zoom
+    var scale, area;
+
+    var clip = d3.geo.clipExtent();
+
+    var zoom = d3.behavior.zoom()
+      .translate(initTranslate)
+      .scale(1)
+      .scaleExtent([1, 8]);
+
+      // .center([mapWidth/2, mapHeight/2])
+
+    function createMapPath (land, boundary, scaleTranslate) {
+      var translate;
+
+      var simplify = d3.geo.transform({
+        point: function (x, y, z) {
+          if(z >= area) {
+            this.stream.point(x * scale + translate[0], y * scale + translate[1]);
+          }
+        }
+      });
+
+      var path = d3.geo.path()
+        .projection({
+          stream: function (s) {
+            return simplify.stream(clip.stream(s));
+          }
+        })
+        .context(context);
+
+      return function (translatePos) {
+          translate = translatePos;
+
+          // console.log(translate[0]*scale);
+
+          context.beginPath();
+          path(land);
+          context.fillStyle = '#cecfd3';
+          context.fill();
+
+          context.beginPath();
+          path(boundary);
+          context.strokeStyle = '#fff';
+          context.stroke();
+      }
+    }
 
     d3.json('/charts/json/world.json', function (err, world) {
       if(err) throw err;
@@ -60,30 +83,18 @@ d3.chart('WorldMapChart', {
         return a !== b;
       });
 
-      function fillMap (translatePos) {
-        translate = translatePos;
-
-        context.beginPath();
-        path(land);
-        context.fillStyle = '#cecfd3';
-        context.fill();
-
-        context.beginPath();
-        path(boundary);
-        context.strokeStyle = '#fff';
-        context.stroke();
-      }
+      var maps = [createMapPath(land, boundary, 1), createMapPath(land, boundary, 2)];
 
       function renderWorld () {
         var zoomTranslate = zoom.translate();
         scale = zoom.scale();
         area = 1/scale/scale;
 
-        console.log(zoomTranslate);
+        console.log(scale);
 
-        if(zoomTranslate[0] > mapWidth) {
+        if(zoomTranslate[0] > mapWidth*scale) {
           zoom.translate([zoomTranslate[0]-mapWidth, zoomTranslate[1]]);
-        } else if (zoomTranslate[0] < -mapWidth) {
+        } else if (zoomTranslate[0] < -mapWidth*scale) {
           zoom.translate([zoomTranslate[0]+mapWidth, zoomTranslate[1]]);
         }
 
@@ -93,12 +104,12 @@ d3.chart('WorldMapChart', {
 
         context.save();
 
-        fillMap(zoomTranslate);
+        maps[0](zoomTranslate);
 
         if(zoomTranslate[0] > 0) {
-          fillMap([zoomTranslate[0]-mapWidth, zoomTranslate[1]]);
+          maps[1]([(zoomTranslate[0]-mapWidth*scale), zoomTranslate[1]]);
         } else {
-          fillMap([zoomTranslate[0]+mapWidth, zoomTranslate[1]]);
+          maps[1]([(zoomTranslate[0]+mapWidth*scale), zoomTranslate[1]]);
         }
 
         context.restore();
